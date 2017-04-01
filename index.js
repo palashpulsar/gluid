50fe89b1f8c654c2c4f68e57d93b53795a89e6d7
Palash Sarkar <palash.sarkar2007@gmail.com>
2017-04-01 12:12:38 +0300

    Basic hex representation html page.

---
+++ index.js
@@ -0,0 +1,62 @@
+$(document).on("pageinit", "#layoutPlot", function(event){
+    d3jsPlot();
+});
+
+function d3jsPlot(){
+
+    // Set the dimension
+    var margin = {top: 20, right: 20, bottom: 50, left: 70},
+                    width = 1300 - margin.left - margin.right,
+                    height = 300 - margin.top - margin.bottom;
+
+    var svg = d3.select("#d3js_Plot").append("svg")
+                                .attr("width", width + margin.left + margin.right)
+                                .attr("height", height + margin.top + margin.bottom)
+                                .append("g")
+                                .attr("transform",
+                                        "translate(" + margin.left + "," + margin.top + ")");
+
+    var randomX = d3.randomNormal(width / 2, 80),
+        randomY = d3.randomNormal(height / 2, 80),
+        points = d3.range(2000).map(function() { return [randomX(), randomY()]; });
+
+    var color = d3.scaleSequential(d3.interpolateLab("white", "steelblue"))
+        .domain([0, 20]);
+
+    var hexbin = d3.hexbin()
+        .radius(20)
+        .extent([[0, 0], [width, height]]);
+
+    var x = d3.scaleLinear()
+        .domain([0, width])
+        .range([0, width]);
+
+    var y = d3.scaleLinear()
+        .domain([0, height])
+        .range([height, 0]);
+
+    svg.append("clipPath")
+        .attr("id", "clip")
+      .append("rect")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.append("g")
+        .attr("class", "hexagon")
+        .attr("clip-path", "url(#clip)")
+      .selectAll("path")
+      .data(hexbin(points))
+      .enter().append("path")
+        .attr("d", hexbin.hexagon())
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .attr("fill", function(d) { return color(d.length); });
+
+    svg.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).tickSizeOuter(-width));
+
+    svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickSizeOuter(-height));
+}
